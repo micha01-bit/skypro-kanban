e0cf81f5aa5b91b2023b178434c777146dd659b2
micha01-bit <ebespalov05@gmail.com>
2025-11-11 01:50:32 +0700

    react_hw-2

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,12 +1,43 @@
-import Card from '../Card/Card.jsx';
+import { useState, useEffect } from "react";
+import Card from "../Card/Card.jsx"; 
 
 function Column() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // задержка 3 секунды
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <> 
-    <div className="main__content">
-        <Card />
-    </div> 
-    </>
+    <div
+      className="main__content"
+      style={{ height: "100vh", position: "relative" }}
+    >
+      {isLoading && (
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            fontSize: "24px",
+            fontWeight: "bold",
+            color: "#555",
+            textAlign: "center",
+            width: "100%",
+            zIndex: 1000 /* Чтобы сообщение было поверх контента */,
+          }}
+        >
+          Данные загружаются
+        </div>
+      )}
+
+      {!isLoading && <Card />}
+    </div>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,45 +1,70 @@
+import { useState } from 'react';
+
 function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <>
       <header className="header">
         <div className="container">
           <div className="header__block">
+            {/* Логотипы */}
             <div className="header__logo _show _light">
               <a href="" target="_self">
-                <img src="images/logo.png" alt="logo"></img>
+                <img src="../images/logo.png" alt="logo" />
               </a>
             </div>
             <div className="header__logo _dark">
               <a href="" target="_self">
-                <img src="images/logo_dark.png" alt="logo"></img>
+                <img src="../images/logo_dark.png" alt="logo" />
               </a>
             </div>
+
+            {/* Навигация */}
             <nav className="header__nav">
               <button className="header__btn-main-new _hover01" id="btnMainNew">
                 <a href="#popNewCard">Создать новую задачу</a>
               </button>
-              <a href="#user-set-target" className="header__user _hover02">
+              
+              {/* Имя пользователя с обработчиком onClick */}
+              <a
+                href="#user-set-target"
+                className="header__user _hover02"
+                onClick={(e) => {
+                  e.preventDefault(); // чтобы избежать перехода по ссылке
+                  toggleModal();
+                }}
+              >
                 Ivan Ivanov
               </a>
-              <div
-                className="header__pop-user-set pop-user-set"
-                id="user-set-target"
-              >
-                {/* <!-- <a href="">x</a> --> */}
-                <p className="pop-user-set__name">Ivan Ivanov</p>
-                <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-                <div className="pop-user-set__theme">
-                  <p>Темная тема</p>
-                  <input
-                    type="checkbox"
-                    className="checkbox"
-                    name="checkbox"
-                  />
+
+              {/* Модальное окно, показывается по состоянию isModalOpen */}
+              {isModalOpen && (
+                <div className="header__pop-user-set pop-user-set" id="user-set-target">
+                  {/* <a href="">x</a> */}
+                  <p className="pop-user-set__name">Ivan Ivanov</p>
+                  <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+                  <div className="pop-user-set__theme">
+                    <p>Темная тема</p>
+                    <input
+                      type="checkbox"
+                      className="checkbox"
+                      name="checkbox"
+                    />
+                  </div>
+                  <button
+                    type="button"
+                    className="_hover03"
+                    onClick={toggleModal} // Закрываем окно при клике
+                  >
+                    Выйти
+                  </button>
                 </div>
-                <button type="button" className="_hover03">
-                  <a href="#popExit">Выйти</a>
-                </button>
-              </div>
+              )}
             </nav>
           </div>
         </div>
